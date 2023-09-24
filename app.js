7ad3a41ff22625f9b469647f7c4d6618ce4e637f
mohamedtolba19 <mohamedtolba9911@gmail.com>
2023-09-24 21:11:47 +0300

    third commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors")
 const { dbConnection } = require('./config/dbconnection');
 const app = express() ;
-const port = 3006 
+const port = 3006 || process.env.PORT ;
 app.use(express.json());
 app.use(cors());
  dbConnection();
